389f90e862f19aedca863bf1e5e5f661f5a35008
Lucie Wolf <lucie@LWMacbook.local>
2025-05-01 22:08:45 +0000

    Adding folders for db stuff

---
+++ adding_to_database/connect.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import MySQLConnection
+from mysql.connector.cursor import MySQLCursor
+from config import Config
+
+
+def get_connection(autocommit: bool = True) -> MySQLConnection:
+    config = Config()
+    db_conf = {
+        "host": config.tidb_host,
+        "port": config.tidb_port,
+        "user": config.tidb_user,
+        "password": config.tidb_password,
+        "database": config.tidb_db_name,
+        "autocommit": autocommit,
+        "use_pure": True
+    }
+
+    if config.ca_path:
+        db_conf["ssl_verify_cert"] = True
+        db_conf["ssl_verify_identity"] = True
+        db_conf["ssl_ca"] = config.ca_path
+    return mysql.connector.connect(**db_conf)
